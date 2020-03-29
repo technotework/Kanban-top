7da7e1a593a1ce5f3c345bb3c92985b3704ff1f6
technote <>
2020-03-29 23:11:01 +0900

    スライド処理

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import 'slick-carousel';
 
 /**
  * ready
@@ -11,12 +12,31 @@ $(() => {
  * 初期化
  */
 function init() {
+  //スライドのセットアップ
+  setupSlide();
+  setupTab();
+}
 
+/**
+ * スライドの設定処理
+ */
+function setupSlide() {
+  const groups = ["#js-tab__board--figures", "#js-tab__multiuser--figures", "#js-tab-realtime--figures"];
+
+  for (let i = 0; i < groups.length; i++) {
+    let $target = $(groups[i]);
+    $target.slick();
+  }
+}
+
+/**
+ * タブの設定処理
+ */
+function setupTab() {
   const targets = ["#js-tab__board", "#js-tab__multiuser", "#js-tab-realtime"];
   changeTab($(targets[0]));
 
   for (let i = 0; i < targets.length; i++) {
-
     $(targets[i]).on("click", (e) => {
       const $target = $(e.target);
       changeTab($target);
@@ -25,14 +45,22 @@ function init() {
   }
 }
 
+
 /**
  * タブのIDを与えるとタブコンテンツを切り替える処理
  * @param {*} $target 
  */
 function changeTab($target) {
+
+  //タブとコンテンツの表示処理
   $("ul.js-tab li a").removeClass("p-feature__tab--current");
   $(".js-tab-item").hide();
   const $myContent = $("#" + $target.attr("id") + "--content");
   $target.addClass("p-feature__tab--current");
   $myContent.show();
+
+  //slickのposition処理のfire
+  const $mySlide = $("#" + $target.attr("id") + "--figures");
+  $mySlide.slick('setPosition');
+
 }
